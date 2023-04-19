ceff030b3f44d9fd52a48e98c881ef87a098cbb5
jameslloydexus <109299375+jameslloydexus@users.noreply.github.com>
2023-04-19 13:38:58 +0200

    Initialize renovate/configure

---
+++ src/main/java/uk/co/exus/templates/javaspringtemplate/JavaSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package uk.co.exus.templates.javaspringtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTemplateApplication.class, args);
+	}
+
+}
